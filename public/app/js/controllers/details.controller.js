d0951d20d9015a8c1ae54e8d5048ceec40106815
Benjamín Martínez Mateos <isc.bkm@gmail.com>
2016-08-19 16:00:40 -0500

    Calculates distances and time from driver to client position

--- public/app/js/controllers/details.controller.js
+++ public/app/js/controllers/details.controller.js
@@ -1,41 +1,93 @@
 (function () {
-   var app = angular.module('parkiller');
+    var app = angular.module('parkiller');
 
-   app. controller('DetailsController', DetailsController)
+    app. controller('DetailsController', DetailsController)
 
-   DetailsController.$inject = ['$scope', '$window', '$timeout'];
+    DetailsController.$inject = ['$scope', '$window', '$timeout', '$http', 'MapService'];
 
-   function DetailsController($scope, $window, $timeout) {
-      console.log('Details controller started');
-      var vm = this;
-      var simulation = io.connect(_BASE_ + ':8080/simulation');
-      vm.markers = [];
-           
-      simulation.on('clear-markers', function (data) {
-         if (data.markers) {
-            vm.markers = data.markers;
-         }
-      });
-
-      simulation.on('marker-update', function (data) {
-         if (data) {
-            console.log('Request update row in table for ' + data.markerIdentifier, data);
-            for (var i = 0; i < vm.markers.length; i++) {
-               if (vm.markers[i].markerIdentifier == data.markerIdentifier) {
-                  $timeout(function () {
-                     vm.markers[i].latitude = data.latitude;
-                     vm.markers[i].longitude = data.longitude;
-                  })
-                  break;
-               }
+    function DetailsController($scope, $window, $timeout, $http, MapService) {
+        console.log('Details controller started');
+        var vm = this;
+        var simulation = io.connect(_BASE_ + ':8080/simulation');
+        vm.markers = [];
+              
+        simulation.on('clear-markers', function (data) {
+            if (data.markers) {
+                vm.markers = data.markers;
+                processMarkersDistanceAndTime();
             }
-         }
-      });
+        });
 
-      $.get('/markers', function(response) {
-         if (response) {
-            vm.markers = response.markers;
-         }
-      });
-   }
+        simulation.on('marker-update', function (data) {
+            if (data) {
+                console.log('Request update row in table for ' + data.markerIdentifier, data);
+                for (var i = 0; i < vm.markers.length; i++) {
+                    if (vm.markers[i].markerIdentifier == data.markerIdentifier) {
+                        $timeout(function () {
+                            vm.markers[i].latitude = data.latitude;
+                            vm.markers[i].longitude = data.longitude;
+
+                            updateDistanceAndTimeInDataTable(vm.markers[i]);
+                        });
+
+                        break;
+                    }
+                }
+            }
+        });
+
+        function processMarkersDistanceAndTime() {
+            for (var i = 0; i < vm.markers.length; i++) {                
+                updateDistanceAndTimeInDataTable(vm.markers[i]);
+            }
+        }
+
+        function updateDistanceAndTimeInDataTable(marker) {
+            var closestMarker;
+            
+            for (var j = 0; j < vm.markers.length; j++) {
+                if (marker.closestMarker == vm.markers[j].markerIdentifier) {
+                    closestMarker = j;
+                    break;
+                }
+            }
+            
+            if (marker.markerType == 'driver' && closestMarker) {
+                var directionsService = new google.maps.DirectionsService(); 
+                var origin = new google.maps.LatLng(marker.latitude, marker.longitude);
+                var destination = new google.maps.LatLng(vm.markers[j].latitude, vm.markers[j].longitude);
+
+                var request = { 
+                            origin: origin, 
+                            destination: destination, 
+                            travelMode: google.maps.DirectionsTravelMode.WALKING 
+                        };
+
+                directionsService.route(request, function(response, status) {
+                    if (status == google.maps.DirectionsStatus.OK) {
+                        var route = response.routes[0];
+
+                        $timeout(function () {
+                            marker.time = route.legs[0].distance.text;
+                            marker.distance = route.legs[0].duration.text;
+                            console.log('Successfully calculated distance and time', marker);
+                        });
+                    } else {
+                        console.log('Can\'t determine steps between points: ' + status);
+                    }
+                });
+            }
+        }
+
+        $http
+            .get('/markers')
+            .then(function(response) {
+                if (response.data) {
+                    vm.markers = response.data.markers;
+                    processMarkersDistanceAndTime();
+                }
+            }, function (error) {
+                console.log('Error retrieving markers from backend');
+            });
+    }
 })();
